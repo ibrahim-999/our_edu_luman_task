e4b4c0c77f20189be54afd7c0366defdb77ecb46
“ibrahim-999” <ibrahimkhalaf99@gmail.com>
2023-08-01 18:45:04 +0300

    [Add] docker files configuration

--- docker/app_install.sh
+++ docker/app_install.sh
@@ -1,4 +1,4 @@
-cp .env.luman ../luman/.env
+cp .env.lumen ../lumen/.env
 docker-compose up -d --build --force-recreate --always-recreate-deps 
 docker-compose exec -T php /bin/sh -c "composer install && \
 php artisan key:generate && \
